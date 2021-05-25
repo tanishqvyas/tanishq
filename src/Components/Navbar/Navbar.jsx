c138691e0e926013b455817762acd52d4a330ac4
tanishqvyas <tanishqvyas069@gmail.com>
2021-05-25 21:25:10 +0530

    Improved Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import styles from "./Navbar.module.css";
 
 import { navLinks } from "../../Data/Navbar";
+import { websiteBaseURL } from "../../Config/Config";
 
 const Navbar = () => {
   return (
     <header className={styles.container}>
-      <div id={styles.name_container}>
+      <a
+      href={websiteBaseURL}
+      id={styles.name_container}
+      className={"no-underline"}
+      >
+        
           Tanishq Vyas
-      </div>
+      </a>
       <nav id={styles.nav_container}>
         {navLinks.map((item, index) => (
           <a className={`${styles.nav_ele} ${"no-underline"}`} key={index} rel="next" href={item.link}>
